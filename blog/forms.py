cd3790feb85eb2671508d4934a30aab3ffa7049f
yuri08011990 <yuraskavinsky@gmail.com>
2020-07-24 15:45:23 +0300

    Connected social media and updated the application form

--- blog/forms.py
+++ blog/forms.py
@@ -10,8 +10,8 @@ class AppicationForm(forms.ModelForm):
 
 
 class RawApplicationForm(forms.Form):
-    name = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder': "Ім'я", 'style': 'width: 333.6px'}))
-    email = forms.EmailField(required=False, label="", widget=forms.TextInput(attrs={'placeholder': "Email", 'style': 'width: 333.6px'}))
-    phone = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder': "Номер телефону", 'style': 'width: 333.6px'}))
-    course = forms.ModelChoiceField(queryset=Course.objects.all().order_by('title'), label='', empty_label='Оберіть курс', widget=forms.Select(attrs={'style': 'width: 333.6px'}))
-    comment = forms.CharField(required=False, label="", widget=forms.Textarea(attrs={'placeholder': "Коментар (не обов'язково)", 'cols': 40, 'rows': 2}))
+    name = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder': "Ім'я", 'style': 'width: 340px'}))
+    email = forms.EmailField(required=False, label="", widget=forms.TextInput(attrs={'placeholder': "Email", 'style': 'width: 340px'}))
+    phone = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder': "Номер телефону", 'style': 'width: 340px'}))
+    course = forms.ModelChoiceField(queryset=Course.objects.all().order_by('title'), label='', empty_label='Оберіть курс', widget=forms.Select(attrs={'style': 'width: 340px'}))
+    comment = forms.CharField(required=False, label="", widget=forms.Textarea(attrs={'placeholder': "Коментар (не обов'язково)", 'rows': 2, 'style': 'width: 340px'}))
